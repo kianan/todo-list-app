4e09a7d0d2ad1130829bd29287e7334c8d5ec187
Chia Kian An <kianan@gmail.com>
2024-10-30 10:53:22 +0800

    feat: add rate limiter to API calls

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
@@ -10,6 +11,16 @@ const { listenForMessages } = require('./pubsub/consumer');
 
 // Middleware to parse JSON
 app.use(express.json());
+
+// Set up rate limiting
+const limiter = rateLimit({
+  windowMs: 2 * 60 * 1000, // 15 minutes
+  max: 15, // Limit each IP to 100 requests per windowMs
+  message: 'Too many requests from this IP, please try again after 15 minutes.',
+});
+
+// Apply rate limiter to all requests
+app.use(limiter);
 
 // Connect to MongoDB
   mongoose.connect(process.env.MONGODB_URI)
@@ -26,7 +37,7 @@ const todoSchema = new mongoose.Schema({
 const Todo = mongoose.model('Todo', todoSchema);
 
 // Create a new todo
-app.post('/todos', async (req, res) => {
+app.post('/todos', limiter, async (req, res) => {
   try {
     const { title, completed, duedate } = req.body;
     const newTodo = new Todo({ title, completed, duedate });
@@ -42,7 +53,7 @@ app.post('/todos', async (req, res) => {
 });
 
 // Retrieve all todos
-app.get('/todos', async (req, res) => {
+app.get('/todos', limiter, async (req, res) => {
   try {
     const todos = await Todo.find();
     res.json(todos);
@@ -65,7 +76,7 @@ app.get('/todos/:id', async (req, res) => {
 });
 
 // Update a specific todo by ID
-app.put('/todos/:id', async (req, res) => {
+app.put('/todos/:id', limiter, async (req, res) => {
   try {
     const { title, completed, duedate } = req.body;
     var updatedTodo = await Todo.findByIdAndUpdate(
@@ -83,7 +94,7 @@ app.put('/todos/:id', async (req, res) => {
 });
 
 // Delete a specific todo by ID
-app.delete('/todos/:id', async (req, res) => {
+app.delete('/todos/:id', limiter, async (req, res) => {
   try {
     const deletedTodo = await Todo.findByIdAndDelete(req.params.id);
     if (!deletedTodo) {
